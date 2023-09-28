4dbb77c0baf858e0d101d1de1cdfa7ad9d0dc10f
Your Name <you@example.com>
2023-09-28 00:09:47 -0600

    Updated extract_hybrid_genes.sh concat_hybrid_genes.fasta notes.txt README.md on 2023-09-28

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# phytoplasma_hybrid_genes
+Name: Kevin Muirhead
+Date: September 28th, 2023
+
+### Instructions on how to install the conda environments for the shell script and python script.
+
+## Install EMBOSS conda environment to get getorf command.
+
+# Create the EMBOSS conda environment.
+conda create --name emboss_env
+
+# Activate the EMBOSS conda environment
+conda activate emboss_env
+
+# Install the EMBOSS conda environment.
+conda install -c bioconda emboss
+
+## Install biopython conda environment to get getorf command.
+
+# Create the biopython conda environment.
+conda create --name biopython_env
+
+# Activate the biopython conda environment
+conda activate biopython_env
+
+# Install the biopython conda environment.
+conda install -c bioconda biopython
+
+
+### How to use the extract_hybrid_genes.sh script.
+
+Use a text editor to edit the input and output directories in the extract_hybrid_genes.sh file.
+
+
--- extract_hybrid_genes.sh
+++ extract_hybrid_genes.sh
@@ -3,11 +3,11 @@
 # Activate the .bashrc source file so that we can use the conda environments in the script.
 source ~/.bashrc
 
-input_dir="/home/kevin.muirhead/phytoHybGenes/gene_assemblies"
-#input_dir="/Users/kevin.muirhead/Desktop/phytoHybGenes/gene_assemblies"
+input_dir="/home/kevin.muirhead/phytoplasma_hybrid_genes/gene_assemblies"
+#input_dir="/Users/kevin.muirhead/Desktop/phytoplasma_hybrid_genes/gene_assemblies"
 
-output_dir="/home/kevin.muirhead/phytoHybGenes"
-#output_dir="/Users/kevin.muirhead/Desktop/phytoHybGenes"
+output_dir="/home/kevin.muirhead/phytoplasma_hybrid_genes"
+#output_dir="/Users/kevin.muirhead/Desktop/phytoplasma_hybrid_genes"
 
 mkdir -p $output_dir
 
@@ -22,6 +22,12 @@ max_size=4000
 
 # Find the top three longest ORFs.
 orf_find=3
+
+# The strain name
+strain_name="BbSP"
+
+# The organism name.
+organism_name="Candidatus Phytoplasma asteris"
 
 # List of gene names to match in the hybrid assembly files and order you want the sequences to be concatenated.
 gene_list=("cpn60" "secY" "secA" "nusA" "tuf")
@@ -39,7 +45,7 @@ join_by_string() {
 gene_regex=$(join_by_string "\|" "${gene_list[@]}")
 
 # The list of hybrid assembly file paths.
-assembly_list_file="${output_dir}/hybrid_assembly_files.txt"
+assembly_list_file="${input_dir}/hybrid_assembly_files.txt"
 
 # Find all the hybrid assembly files and write the file paths to the assembly_list_file.
 #find $input_dir -name "*\.txt" | grep "cpn60\|secY\|secA\|nusA\|tuf" > $assembly_list_file
@@ -52,22 +58,40 @@ do
 	# Get the gene name. Use basename to get the filename. Remove the ".txt" extension using sed. Grab 	
 	gene_name=$(basename $fasta_file | sed 's/\.txt//g' | sed 's/.*\(cpn60\|secY\|secA\|nusA\|tuf\).*/\1/g');
 	
-	#echo $gene_name;
+	echo $gene_name;
 
 	#exit 0;
 	
 	# The path of the gene output file.
 	gene_fasta_outfile="${gene_output_dir}/${gene_name}.fasta"
+	
+	echo $gene_fasta_outfile
 
 	# Activate the EMBOSS conda environment.
 	conda activate emboss_env
 
 	# Use the getorf command to get ORFs from each assembly.
+	echo -e "getorf -sequence $fasta_file -minsize $min_size -maxsize $max_size -find $orf_find -methionine N -outseq $gene_fasta_outfile"
 	getorf -sequence $fasta_file -minsize $min_size -maxsize $max_size -find $orf_find -methionine N -outseq $gene_fasta_outfile
 
 done
 
 gene_fasta_list_file="${gene_output_dir}/hybrid_genes_files.txt"
 
-$(find $gene_output_dir -name "*\.fasta" -type f | grep "${gene_regex}" > $gene_fasta_list_file)
+# The list of hybrid gene file paths.
+find $gene_output_dir -name "*\.fasta" -type f | grep "${gene_regex}" > $gene_fasta_list_file
+
+# Get a concatenated ordered list of gene names separated by commas.
+gene_name_list=$(join_by_string "," "${gene_list[@]}")
+
+# Activate the biopython conda environment.
+conda activate biopython_env
+
+echo "Concatinating ${gene_name_list} files..."
+
+# Concatenate hybrid gene panel sequences in order of the gene_name_list.
+echo -e "python concat_seqs_order.py --fasta_file_list_infile $gene_fasta_list_file --gene_name_list $gene_name_list --strain_name $strain_name --organism_name  $organism_name --output_dir $output_dir"
+python concat_seqs_order.py --fasta_file_list_infile $gene_fasta_list_file --gene_name_list $gene_name_list --strain_name "\"${strain_name}\"" --organism_name "\"${organism_name}\"" --output_dir $output_dir
+
+
 
